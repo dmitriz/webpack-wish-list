3c80c38882ea332cf20f97fe14cc508927ebb52c
dmitriz <dmitri14@gmail.com>
2016-01-10 12:08:12 +0000

    Fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
-module.export = {
-    context: __dirname + "/app",
-    entry: "./entry",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    }
-}
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+};
